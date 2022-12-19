a5ac6a9f9cc5a0489dbfd4dbf6cad531490e5328
Hamza Koc <hamzakooc@gmail.com>
2022-12-18 20:25:17 -0500

    first file

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
   name     = var.rg_name
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg_new" {
+resource "azurerm_resource_group" "rg2" {
   name     = var.rg_name
   location = "West Europe"
 }
